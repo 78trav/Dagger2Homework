b7edec7c7f7f498b006cfff3b6097fdf5c719c9e
78trav <trav78@yandex.ru>
2023-08-15 19:12:44 +0300

    ДЗ dagger2

--- app/src/main/java/ru/otus/daggerhomework/MainActivity.kt
+++ app/src/main/java/ru/otus/daggerhomework/MainActivity.kt
@@ -1,12 +1,61 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var activityComponent: MainActivityComponent
+
+    fun getComponent() = activityComponent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        activityComponent = DaggerMainActivityComponent
+            .builder()
+            .setApplicationComponent((application as App).getComponent())
+            .setActivityContext(this)
+            .build()
+
         setContentView(R.layout.activity_main)
     }
+}
+
+@ActivityScope
+@Component(
+    dependencies = [ApplicationComponent::class],
+    modules = [MainActivityModule::class]
+)
+interface MainActivityComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun setActivityContext(@ActivityContext context: Context): Builder
+        fun setApplicationComponent(applicationComponent: ApplicationComponent): Builder
+        fun build(): MainActivityComponent
+    }
+
+    @ActivityContext
+    fun provideMainActivityContext(): Context
+
+    @ApplicationContext
+    fun provideApplicationContext(): Context
+
+    fun provideColorState(): MutableStateFlow<Int>
+
+}
+
+@Module
+object MainActivityModule {
+    @ActivityScope
+    @Provides
+    fun providesColorState(): MutableStateFlow<Int> = MutableStateFlow(0)
 }
--- app/src/main/java/ru/otus/daggerhomework/ViewModelProducer.kt
+++ app/src/main/java/ru/otus/daggerhomework/ViewModelProducer.kt
@@ -3,14 +3,33 @@ package ru.otus.daggerhomework
 import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
 
-class ViewModelProducer(
-    private val colorGenerator: ColorGenerator,
-    private val context: Context
-) {
+class ViewModelProducer (
+    private val context: Context,
+    private val colorGenerator: IColorGenerator,
+    private val color: MutableStateFlow<Int>
+) : ViewModel() {
 
     fun generateColor() {
         if (context !is FragmentActivity) throw RuntimeException("Здесь нужен контекст активити")
-        Toast.makeText(context, "Color sent", Toast.LENGTH_LONG).show()
+        Toast.makeText(context, "Color sent", Toast.LENGTH_SHORT).show()
+        color.value = colorGenerator.generateColor()
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+@FragmentScope
+class ViewModelProducerFactory @Inject constructor(
+    @ActivityContext private val context: Context,
+    private val colorGenerator: IColorGenerator,
+    private val color: MutableStateFlow<Int>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ViewModelProducer(context, colorGenerator, color) as T
     }
 }
